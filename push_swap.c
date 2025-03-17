d683fe78747bd9cbe082fd3c6fdaa60ef7f84cbc
ilyasguney <ilyasguney105@gmail.com>
2025-03-18 01:21:22 +0300

    first commit

--- push_swap.c
+++ push_swap.c
@@ -3,16 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iguney <iguney@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ilyas-guney <ilyas-guney@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/18 13:37:42 by iguney            #+#    #+#             */
-/*   Updated: 2025/03/11 20:47:33 by iguney           ###   ########.fr       */
+/*   Updated: 2025/03/17 18:38:17 by ilyas-guney      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main()
+int main(int ac, char *av[])
 {
+	t_stack *stack_a;
+	t_stack *stack_b;
 	
-}
+	stack_a = malloc(sizeof(t_stack));
+	stack_b = malloc(sizeof(t_stack));
+	if (!stack_a || !stack_b)
+		return (0);
+	if (ac != 1)
+	{
+		
+	}
+	ft_printf("\n");
+	
+}@
--- push_swap.h
+++ push_swap.h
@@ -1,7 +1,17 @@
-#ifndef push_swap_H
-#define push_swap_H
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+# include <stdlib.h>
 
+typedef struct s_stack
+{
+	int    			data;
+	struct s_stack	*next;
+	struct s_stack	*pre;
+	
+} t_stack;
 
 #endif
 
